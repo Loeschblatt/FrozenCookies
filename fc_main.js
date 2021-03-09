35f58a9f0804d5f4aae8e84add25965e900e44ff
Loeschblatt <80174853+Loeschblatt@users.noreply.github.com>
2021-03-09 13:57:51 +0100

    Update fc_main.js

--- fc_main.js
+++ fc_main.js
@@ -2405,8 +2405,8 @@ function autoCookie() {
         // This apparently *has* to stay here, or else fast purchases will multi-click it.
         if (goldenCookieLife() && FrozenCookies.autoGC) {
             for (var i in Game.shimmers) {
-                if (Game.shimmers[i].type == 'golden') {
-                    Game.shimmers[i].pop();
+                if (Game.shimmers[i].type == 'golden' && Game.shimmers[i].force != 'cookie storm drop') {
+					Game.shimmers[i].pop();
                 }
             }
         }
